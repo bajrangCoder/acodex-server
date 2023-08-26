155c0f3cb441ba4c88d2db5757f88c3aa28c601a
Raunak Raj <nkray21111983@gmail.com>
2023-08-26 10:20:31 +0530

    v1.0.3

--- index.js
+++ index.js
@@ -4,34 +4,43 @@ const { WebSocketServer } = require("ws");
 const pty = require("node-pty");
 
 function startWebSocketServer() {
-  let port = parseInt(process.env.WS_PORT || process.env.PORT || 8767);
+    let port = parseInt(process.env.WS_PORT || process.env.PORT || 8767);
 
-  const server = new WebSocketServer({
-    port: port,
-  });
-  
-  server.on("connection", function (connection) {
-    const shell = pty.spawn("bash", [], {
-      name: "xterm-256color",
-      cwd: process.env.HOME,
-      env: process.env,
+    const server = new WebSocketServer({
+        port: port,
     });
-  
-    connection.on("message", function (message) {
-      const command = typeof message === "string" ? message.trim() : message.toString("utf8").trim();
-      shell.write(command + "\r");
-    });
-  
-    shell.on("data", function (data) {
-      connection.send(data);
-    });
-  
-    shell.on("exit", function () {
-      connection.close();
-    });
-  });
 
-  console.log(`AcodeX Server started on port: ${port}`);
+    server.on("connection", function (connection) {
+        const shell = pty.spawn(process.env.SHELL || "bash", [], {
+            name: "xterm-256color",
+            cwd: process.env.HOME,
+            env: process.env,
+        });
+
+        connection.on("message", function (message) {
+            const command =
+                typeof message === "string"
+                    ? message.trim()
+                    : message.toString("utf8").trim();
+            if (command.startsWith("acodeXResize")) {
+                const newCols = parseInt(command.split(":")[1].split(",")[0]);
+                const newRows = parseInt(command.split(":")[1].split(",")[1]);
+                shell.resize(newCols, newRows);
+            } else {
+                shell.write(command + "\r");
+            }
+        });
+        
+        shell.on("data", function (data) {
+            connection.send(data)
+        });
+
+        shell.on("exit", function () {
+            connection.close();
+        });
+    });
+
+    console.log(`AcodeX Server started on port: ${port}`);
 }
 
 // Export the function to make it accessible from other modules
@@ -39,5 +48,5 @@ module.exports = startWebSocketServer;
 
 // If this script is executed directly from the command line, start the WebSocket server
 if (require.main === module) {
-  startWebSocketServer();
+    startWebSocketServer();
 }
