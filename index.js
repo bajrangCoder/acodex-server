f4915752dc647651d347f7eb1c5693495d259444
Raunak Raj <nkray21111983@gmail.com>
2023-10-08 17:06:49 +0530

    Patch v1.0.7

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 #!/usr/bin/env node
 const { Command } = require("commander");
 const startServer = require("./terminalServer.js");
-const startLspServer = require("./lspServer.js");
+//const startLspServer = require("./lspServer.js");
 const program = new Command();
 
 program
     .name("acodeX-server")
     .description("CLI of AcodeX Acode plugin")
-    .version("1.0.5")
+    .version("1.0.7")
     .action(() => {
         startServer();
     });
 
 // note: Currently its not for normal user
-program
+/*program
     .command("lsp")
     .description("Starts lsp server for Acode")
     .action(() => {
         startLspServer();
         console.log("Started lsp server")
     });
-
+*/
 program.parse();
